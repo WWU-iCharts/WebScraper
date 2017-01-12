eb37aec709a0d57b5d265a13aae6adfb7e9b6914
Walker60 <Chris.ak.walker@gmail.com>
2017-01-12 14:46:33 -0800

    added getting start and end dates for charts

--- webscrape.py
+++ webscrape.py
@@ -1,3 +1,6 @@
+#Icharts
+#Written by Chris Walker
+
 from bs4 import BeautifulSoup
 import requests
 import smtplib
@@ -5,14 +8,31 @@ import smtplib
 
 
 def webscrape():
-    url = 'http://www.faa.gov/air_traffic/flight_info/aeronav/digital_products/vfr/?viewType=Print&viewClass=Print'
-    response = requests.get(url)
-    html = response.text
-    soup = BeautifulSoup(html, 'lxml')
-    table = soup.find(class_='striped')
 
+    #Error handling if FAA website has changed
     try:
+        #Open table on FAA website conataining Chart data
+        url = 'http://www.faa.gov/air_traffic/flight_info/aeronav/digital_products/vfr/?viewType=Print&viewClass=Print'
+        response = requests.get(url)
+        html = response.text
+        soup = BeautifulSoup(html, 'lxml')
+        table = soup.find(class_='striped')
         table.find_all('tr')
+
+        #Put city,version and download link into temp file
+        map_data = []
+        temp = open("temp", 'w+')
+        temp.truncate()
+
+        for row in table.find_all('tr')[1:]:
+            col = row.find_all('td')
+            city = col[0].string.strip()
+
+            link = col[1].find('a').get('href')
+            version = col[1].get_text().split()
+            version = version[0]
+            temp.write(city + ',' + version + ',' + link + '\n')
+
     except Exception, e:
         server = smtplib.SMTP('smtp.gmail.com', 587)
 
@@ -21,22 +41,8 @@ def webscrape():
         server.ehlo()
 
         server.login("ichartgroup@gmail.com", "icharts321")
-        msg = "Webscraper Failed, FAA website might have changed"
+        msg = "Webscraper Failed: " + e
         server.sendmail("ichartgroup@gmail.com", "walker60@wwu.edu", msg)
         return
-
-    map_data = []
-    temp = open("temp", 'w+')
-    temp.truncate()
-
-    for row in table.find_all('tr')[1:]:
-        col = row.find_all('td')
-        city = col[0].string.strip()
-
-        link = col[1].find('a').get('href')
-        version = col[1].get_text().split()
-        version = version[0]
-        temp.write(city + ',' + version + ',' + link + '\n')
-
 
 webscrape()
